793eac5338fd80f609edb3c1d8220ed3451357dc
Jon Davis <jonerikdavis@gmail.com>
2020-05-26 17:26:11 -0700

    what is req

--- src/libs/mobileDetection.js
+++ src/libs/mobileDetection.js
@@ -2,8 +2,7 @@
 // compares the user agent of the caller against a regex
 // This regex comes from http://detectmobilebrowsers.com/
 module.exports = function isCallerMobile(req) {
-  if (!req) return false;
-
+  console.log('req is', req);
   const ua = req.headers['user-agent'].toLowerCase();
   const isMobile =
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
