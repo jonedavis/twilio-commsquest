bf999cb928b61a7ebca73e7b5e9c3a285e169043
Jon Davis <jonerikdavis@gmail.com>
2020-05-27 16:53:45 -0700

    cypress ci

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -10,64 +10,23 @@ describe('/', () => {
     cy.visit('/');
   });
 
-  it('contains title', () => {
-    cy.get('h1').contains('Template App');
+  it('contains input field', () => {
+    cy.get('#txtPhoneNumber').should('exist');
   });
 
-  it('contains a link to /example', () => {
-    const link = cy.get('a[href="/example"]');
-    link.contains('/example');
-  });
-
-  it('sends an sms', () => {
+  it('gets an access token', () => {
+    cy.get('#txtPhoneNumber').type('15555555555');
     cy.server();
-    cy.route('POST', '/send-sms', 'fixture:send-sms.json').as(
-      'sendSmsActivity'
-    );
+    cy.route('GET', '/token/15555555555', 'fixture:token.json').as('token');
+    cy.get('#btnStart').click();
 
-    cy.get('#toInput')
-      .clear()
-      .type('+12223334444');
-    cy.get('#bodyInput')
-      .clear()
-      .type('This is a test message');
-    cy.get('#smsForm').submit();
-    cy.wait('@sendSmsActivity')
-      .its('requestBody')
-      .should('deep.equal', {
-        to: '+12223334444',
-        body: 'This is a test message',
-      });
-    cy.get('#dialogTitle').contains('SMS Sent!');
-    const dialog = cy.get('#dialog');
-    dialog.should('have.class', 'alert-success');
-    dialog.should('not.have.class', 'd-none');
-    cy.get('#dialogContent').contains('Your SMS has been sent!');
-  });
-
-  it('shows error message', () => {
-    cy.server();
-    cy.route('POST', '/send-sms', 'fixture:error-send-sms.json').as(
-      'sendSmsActivity'
-    );
-
-    cy.get('#toInput')
-      .clear()
-      .type('+12223339999');
-    cy.get('#bodyInput')
-      .clear()
-      .type('This is a test message with the intention to fail');
-    cy.get('#smsForm').submit();
-    cy.wait('@sendSmsActivity')
-      .its('requestBody')
-      .should('deep.equal', {
-        to: '+12223339999',
-        body: 'This is a test message with the intention to fail',
-      });
-    cy.get('#dialogTitle').contains('Error');
-    const dialog = cy.get('#dialog');
-    dialog.should('have.class', 'alert-danger');
-    dialog.should('not.have.class', 'd-none');
-    cy.get('#dialogContent').contains('Oh no :( something went wrong.');
+    cy.wait('@token').then(xhr => {
+      assert.equal(
+        xhr.response.body.identity,
+        '15555555555',
+        'Equals Identity'
+      );
+      assert.isNotNull(xhr.response.body.token, 'Contains Access Token');
+    });
   });
 });
